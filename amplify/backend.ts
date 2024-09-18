03b053902dd987478ece0151349bb5c68c97a971
aaagius <aaagius@amazon.com>
2024-09-18 14:48:06 +0200

    Add bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,9 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { firstBucket, secondBucket } from "./storage/resource";
-import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { data } from "./data/resource";
 import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Stack } from "aws-cdk-lib";
 
 
 const backend = defineBackend({
@@ -10,15 +11,31 @@ const backend = defineBackend({
   data,
   firstBucket, 
   secondBucket,
-  generateHaikuFunction,
 });
 
-backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+const MODEL_ID = "anthropic.claude-3-haiku-20240307-v1:0";
+
+const bedrockDataSource = backend.data.addHttpDataSource(
+  "BedrockDataSource",
+  "https://bedrock-runtime.us-east-1.amazonaws.com",
+  {
+    authorizationConfig: {
+      signingRegion: Stack.of(backend.data).region,
+      signingServiceName: "bedrock",
+    },
+  }
+);
+
+bedrockDataSource.grantPrincipal.addToPrincipalPolicy(
   new PolicyStatement({
     effect: Effect.ALLOW,
     actions: ["bedrock:InvokeModel"],
     resources: [
-      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+      `arn:aws:bedrock:${Stack.of(backend.data).region}::foundation-model/${MODEL_ID}`,
     ],
   })
 );
+
+backend.data.resources.cfnResources.cfnGraphqlApi.environmentVariables = {
+  MODEL_ID
+}
