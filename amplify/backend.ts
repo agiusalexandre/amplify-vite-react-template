7e0cbe61fdc0d8e8f76ec373960ffba2e3481639
aaagius <aaagius@amazon.com>
2024-09-18 14:13:13 +0200

    Add Bedrock

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
 import { firstBucket, secondBucket } from "./storage/resource";
+import { data, MODEL_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 
 const backend = defineBackend({
@@ -9,4 +10,15 @@ const backend = defineBackend({
   data,
   firstBucket, 
   secondBucket,
+  generateHaikuFunction,
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: ["bedrock:InvokeModel"],
+    resources: [
+      `arn:aws:bedrock:*::foundation-model/${MODEL_ID}`,
+    ],
+  })
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css'
 import { Flex, Tabs } from '@aws-amplify/ui-react';
 import { FileUploader } from '@aws-amplify/ui-react-storage';
+import { Amplify } from "aws-amplify";
 
 
 const client = generateClient<Schema>();
 
+const { data, errors } = await client.queries.generateHaiku({
+  prompt: "Frank Herbert's Dune",
+});
+
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
   const [uploadedFiles] = useState<string[]>([]);
+
+  const [prompt, setPrompt] = useState<string>("");
+  const [answer, setAnswer] = useState<string | null>(null);
+
+  const sendPrompt = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { data, errors } = await client.queries.generateHaiku({
+      prompt,
+    });
+
+    if (!errors) {
+      setAnswer(data);
+      setPrompt("");
+    } else {
+      console.log(errors);
+    }
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -39,7 +62,7 @@ function App() {
               <Tabs.List spacing="relative">
                 <Tabs.Item value="1">First</Tabs.Item>
                 <Tabs.Item value="2">Upload Images</Tabs.Item>
-                <Tabs.Item value="3">Really long title for demonstration</Tabs.Item>
+                <Tabs.Item value="3">Bedrock</Tabs.Item>
               </Tabs.List>
               <Tabs.Panel value="1">
                 <h1>My todos</h1>
@@ -72,8 +95,24 @@ function App() {
 
               </Tabs.Panel>
               <Tabs.Panel value="3">
+                <div>
+                  <h1 className="text-3xl font-bold text-center mb-4">Haiku Generator</h1>
 
-                
+                  <form className="mb-4 self-center max-w-[500px]" onSubmit={sendPrompt}>
+                    <input
+                      className="text-black p-2 w-full"
+                      placeholder="Enter a prompt..."
+                      name="prompt"
+                      value={prompt}
+                      onChange={(e) => setPrompt(e.target.value)}
+                    />
+                  </form>
+
+                  <div className="text-center">
+                    <pre>{answer}</pre>
+                  </div>
+                </div>
+
               </Tabs.Panel>
             </Tabs.Container>
           </Flex>
